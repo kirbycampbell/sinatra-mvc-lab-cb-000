4f8bf4d12f60149c02e71f341566fe6a545ed488
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-23 23:48:31 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,10 +13,11 @@ class PigLatinizer
     words.each do |word|
       case
       when word.match(/^aeiouAEIOU/)
-        word += "way"
-        finished_words << word
+        new_word = word + "way"
+        finished_words << new_word
       end
     end
+    finished_words.join(",")
   end
 
 end
