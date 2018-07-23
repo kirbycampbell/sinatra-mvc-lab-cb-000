b64a59adaaff97bf7daba3e00d7480a7b13edd72
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-23 23:45:25 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,22 @@
 class PigLatinizer
 
+  attr_reader :text
 
+  def initialize(text)
+      @text = text.downcase
+  end
+
+  def convert_to_piglatin
+    words = text.split(" ")
+    finished_words = []
+
+    words.each do |word|
+      case
+      when word.match(/^aeiouAEIOU/)
+        word += "way"
+        finished_words << word
+      end
+    end
+  end
 
 end
