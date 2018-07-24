99e061b6c9eb6ebf23787394ef7e3b453e4b96da
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-24 02:04:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
 
   get '/' do
-    status 200
+
     erb :user_input
 
   end
 
   post '/piglatinize' do
-    @piglatinized_text = PigLatinizer.new(params[:words])
+    pig_latin = PigLatinizer.new
+    @piglatinized_text = pig_latin.convert_to_piglatin(params[:words])
 
-    erb :piglatinize
+    erb :results
   end
 end
